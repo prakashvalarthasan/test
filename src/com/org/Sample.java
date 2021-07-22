db9e542b7ac6e86f6860c5164700a193522afd58
prakashvalarthasan <grittierprakash1@gmail.com>
2021-07-22 23:16:42 +0530

    praki done

--- src/com/org/Sample.java
+++ src/com/org/Sample.java
@@ -4,5 +4,13 @@ public class Sample {
 public static void main(String[] args) {
 	String s ="java";
 	System.out.println(s.length());
+    String s1="";
+	char[] cs = s.toCharArray();
+	for (char c : cs) {
+		if(c=='a') {
+			s1+=s1+c;
+		}
+		System.out.println(s1);
+	}
 }
 }
